32a4841d7933ad177cc0579081138fa654dafe6c
Lucas Martins <lucasifbaeuna@gmail.com>
2025-09-19 21:43:41 -0300

    feat: create component list details

---
+++ src/app/pages/detail-investment-page/components/list-details/list-details.component.ts
@@ -0,0 +1,25 @@
+import { STRING_CONSTANTS } from './../../../../shared/constants/string-contants';
+import { Component, input } from '@angular/core';
+import { InvestmentResponse } from '../../../home-page/interface/investment-response';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-list-details',
+  imports: [MatListModule, MatButtonModule, DatePipe],
+  templateUrl: './list-details.component.html',
+  styleUrl: './list-details.component.scss',
+})
+export class ListDetailsComponent {
+  listDetailsData = input<InvestmentResponse | null>();
+  detailsPageTitle = STRING_CONSTANTS.DETAIL_PAGE_TITLE;
+  detailsPageRating = STRING_CONSTANTS.DETAIL_PAGE_RATING;
+  detailsPageLiquidity = STRING_CONSTANTS.DETAIL_PAGE_LIQUIDITY;
+  detailsPageAccumulatedYield = STRING_CONSTANTS.DETAIL_PAGE_ACCUMULATED_YIELD;
+  detailsPageAnnualYield = STRING_CONSTANTS.DETAIL_PAGE_ANNUAL_YIELD;
+  detailsPageVolatility = STRING_CONSTANTS.DETAIL_PAGE_VOLATILITY;
+  detailsPageRedemptionAvailable = STRING_CONSTANTS.DETAIL_PAGE_REDEMPTION_AVAILABLE;
+  detailsPageMinimumRedemptionDate = STRING_CONSTANTS.DETAIL_PAGE_MINIMUM_REDEMPTION_DATE;
+  detailsPageButton = STRING_CONSTANTS.DETAIL_PAGE_BUTTON;
+}
